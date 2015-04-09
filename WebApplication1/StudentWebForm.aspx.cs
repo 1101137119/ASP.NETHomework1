0bdd96102947dc3c99b6ac19792193961e895209
1101137119 <high820904@yahoo.com.tw>
2015-04-09 14:29:27 +0800

    commit1

---
+++ WebApplication1/StudentWebForm.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+
+namespace WebApplication1
+{
+    public partial class StudentWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Students where Name like @name";
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + txtSearch.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        gvResult.DataSource = ds;
+                        gvResult.DataMember = "Table";
+                        gvResult.DataBind();
+                        da.Fill(ds);
+                    }
+                }
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Students";
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + txtSearch.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables["Table"];
+                        DataRow dr = dt.NewRow();
+                        
+                        dr["Name"] = txtName.Text;
+                        dr["Address"] = txtAdd.Text;
+                        dr["Phone"] = txtPhone.Text;
+                        
+                        dt.Rows.Add(dr);
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Students";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("StudentID = {0}", txt_SID.Text)).First();
+                        dr["Name"] = txt_SName.Text;
+                        dr["Address"] = txt_SAdd.Text;
+                        dr["Phone"] = txt_SPhone.Text;
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ADODBConnectionString2"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Students";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("StudentID = {0}", txtD_ID.Text)).First();
+                        dr.Delete();
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication1/StudentWebForm.aspx.designer.cs
@@ -0,0 +1,168 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     對這個檔案所做的變更可能會造成錯誤的行為，而且如果重新產生程式碼，
+//     所做的變更將會遺失。 
+// </自動產生的>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class StudentWebForm {
+        
+        /// <summary>
+        /// form1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// SqlDataSource1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// txtSearch 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSearch;
+        
+        /// <summary>
+        /// btnSearch 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSearch;
+        
+        /// <summary>
+        /// gvResult 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvResult;
+        
+        /// <summary>
+        /// txtID 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtID;
+        
+        /// <summary>
+        /// txtName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// txtAdd 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAdd;
+        
+        /// <summary>
+        /// txtPhone 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPhone;
+        
+        /// <summary>
+        /// btnAdd 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdd;
+        
+        /// <summary>
+        /// txt_SID 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_SID;
+        
+        /// <summary>
+        /// txt_SName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_SName;
+        
+        /// <summary>
+        /// txt_SAdd 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_SAdd;
+        
+        /// <summary>
+        /// txt_SPhone 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_SPhone;
+        
+        /// <summary>
+        /// btnEdit 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEdit;
+        
+        /// <summary>
+        /// txtD_ID 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtD_ID;
+        
+        /// <summary>
+        /// btnDelete 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 將移動欄位宣告從設計檔案修改為程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDelete;
+    }
+}
